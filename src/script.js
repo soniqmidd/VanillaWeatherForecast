81244132c369b47769bc2aebdcaf14074480ebea
soniqmidd <soniqmidd@users.noreply.github.com>
2023-08-28 22:04:43 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -25,25 +25,25 @@ function formatDate(timestamp) {
 
 //display forecast
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
   let days = ["Thu", "Fri", "Sat", "Sun"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
+      <div class="weather-forecast-date">${forecastDay.dt}</div>
       <img
-        src="https://openweathermap.org/img/wn/50d@2x.png"
+        src="https://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
         alt=""
         width="42"
       />
       <div class="weather-forecast-temperatures">
-        <span class="weather-forecast-temperatures-max">18&deg</span>
-        <span class="weather-forecast-temperatures-min">12&deg</span>
+        <span class="weather-forecast-temperatures-max">${forecastDay.temp.max}&deg</span>
+        <span class="weather-forecast-temperatures-min">${forecastDay.temp.min}&deg</span>
       </div>
     </div>
   `;
